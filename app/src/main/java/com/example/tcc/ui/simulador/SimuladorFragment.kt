eb599813cc6624fe0f23d33e60c3a4337612ce4f
F9RR <gabrielozorio@poli.ufrj.br>
2024-09-25 11:09:38 -0300

    inicio ajustes ediçao de netlist

--- app/src/main/java/com/example/tcc/ui/simulador/SimuladorFragment.kt
+++ app/src/main/java/com/example/tcc/ui/simulador/SimuladorFragment.kt
@@ -18,7 +18,11 @@ class SimuladorFragment : Fragment() {
     private lateinit var editTextUsuario: EditText
     private lateinit var editTextServidor: EditText
     private lateinit var editTextSenha: EditText
+    private lateinit var editTextNetlist: EditText
     private lateinit var buttonConectar: Button
+    private lateinit var buttonAddResistor: Button
+    private lateinit var buttonAddCapacitor: Button
+    private lateinit var buttonAddIndutor: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,34 +32,24 @@ class SimuladorFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_simulador, container, false)
 
         // Inicialize os elementos de UI
-        editTextUsuario = root.findViewById(R.id.editTextText)
-        editTextServidor = root.findViewById(R.id.editTextText2)
-        editTextSenha = root.findViewById(R.id.editTextTextPassword)
-        buttonConectar = root.findViewById(R.id.button)
+        editTextUsuario = root.findViewById(R.id.editTextUsuario)
+        editTextServidor = root.findViewById(R.id.editTextServidor)
+        editTextSenha = root.findViewById(R.id.editTextSenha)
+        editTextNetlist = root.findViewById(R.id.editTextNetlist)
+        buttonConectar = root.findViewById(R.id.buttonConectar)
+        buttonAddResistor = root.findViewById(R.id.buttonAddResistor)
+        buttonAddCapacitor = root.findViewById(R.id.buttonAddCapacitor)
+        buttonAddIndutor = root.findViewById(R.id.buttonAddIndutor)
 
-        // Configure o listener do botão
+        // Configure o listener do botão conectar
         buttonConectar.setOnClickListener {
             val usuario = editTextUsuario.text.toString()
             val servidor = editTextServidor.text.toString()
             val senha = editTextSenha.text.toString()
+            val netlistContent = editTextNetlist.text.toString()
 
             // Verifique se todos os campos foram preenchidos
-            if (usuario.isNotEmpty() && servidor.isNotEmpty() && senha.isNotEmpty()) {
-                // Conteúdo do Netlist
-                val netlistContent = """
-                    * Divisor de Tensão Simples
-                    V1 N001 0 DC 10
-                    R1 N001 N002 5
-                    R2 N002 0 5
-
-                    .control
-                    op
-                    print all
-                    .endc
-
-                    .end
-                """.trimIndent()
-
+            if (usuario.isNotEmpty() && servidor.isNotEmpty() && senha.isNotEmpty() && netlistContent.isNotEmpty()) {
                 // Conecte-se ao servidor via SSH
                 CoroutineScope(Dispatchers.IO).launch {
                     val ssh = SSH()
@@ -65,6 +59,19 @@ class SimuladorFragment : Fragment() {
             } else {
                 // Mostre uma mensagem de erro ou faça alguma outra ação adequada
             }
+        }
+
+        // Configure os listeners dos botões de adicionar componentes
+        buttonAddResistor.setOnClickListener {
+            editTextNetlist.append("\nR? N? N? ?")
+        }
+
+        buttonAddCapacitor.setOnClickListener {
+            editTextNetlist.append("\nC? N? N? ?")
+        }
+
+        buttonAddIndutor.setOnClickListener {
+            editTextNetlist.append("\nL? N? N? ?")
         }
 
         return root
